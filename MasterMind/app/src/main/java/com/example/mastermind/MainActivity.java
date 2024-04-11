8e4cdf5513e21ec74407abc5106ecd2ad66e2083
Chuck66SabbacPlayer <129320352+Chuck66SabbacPlayer@users.noreply.github.com>
2024-04-11 18:56:32 -0400

    Update MainActivity.java

--- MasterMind/app/src/main/java/com/example/mastermind/MainActivity.java
+++ MasterMind/app/src/main/java/com/example/mastermind/MainActivity.java
@@ -1,24 +1,52 @@
 package com.example.mastermind;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnJouer;
+    private Button btnConfig;
+    private Button btnHistorique;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnJouer = findViewById(R.id.btnJouer);
+        btnConfig = findViewById(R.id.btnConfig);
+        btnHistorique = findViewById(R.id.btnHistorique);
+
+        btnJouer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Ouvre l'activité JeuActivity
+                Intent intent = new Intent(MainActivity.this, JeuActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnConfig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Ouvre l'activité Reglage
+                Intent intent = new Intent(MainActivity.this, ReglageActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnHistorique.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Ouvre l'activité Parametre
+                Intent intent = new Intent(MainActivity.this, Parametre.class);
+                startActivity(intent);
+            }
         });
     }
 }
